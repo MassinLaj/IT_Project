5b09979e779b27769136cea40aa1aafdb472f812
Massin Lajlufi <s121017@ap.be>
2023-05-15 21:32:20 +0200

    contact change to index en wat added

--- index.ts
+++ index.ts
@@ -8,6 +8,10 @@ app.get("/contact", (req, res) =>{
     res.render("contact");
 });
 
+app.post("/contact", (req, res) => {
+    res.render("emailsent");
+});
+
 app.listen(app.get("port"), () => {
     console.log(`Web app has started on http://localhost:${app.get("port")}`);
-})
+});
