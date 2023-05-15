c2a42a009e011c4e3379b6f494752c4aec49f3b2
Massin Lajlufi <s121017@ap.be>
2023-05-16 00:26:53 +0200

    login register en contact ejs done

---
+++ index.ts
@@ -0,0 +1,49 @@
+import express from "express"
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use('/public', express.static('public'));
+
+app.set("port", 3000);
+app.set("view engine", "ejs");
+
+//contact start
+app.get("/contact", (req, res) =>{
+    res.render("contact");
+});
+
+app.post("/contact", (req, res) => {
+
+    console.log(req.body);
+
+});
+//contact end
+
+//login start
+app.get("/login", (req, res) =>{
+    res.render("login");
+});
+
+app.post("/login", (req, res) => {
+
+    console.log(req.body);
+
+});
+//login end
+
+//register start
+app.get("/register", (req, res) =>{
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+
+    console.log(req.body);
+
+});
+//register end
+
+app.listen(app.get("port"), () => {
+    console.log(`Web app has started on http://localhost:${app.get("port")}`);
+});
